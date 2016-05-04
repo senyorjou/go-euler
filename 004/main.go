1e61371a7749a773f2fbcdd3288a04f29c83e677
Marc Jou <smyslov@gmail.com>
2016-05-04 11:40:07 +0200

    Added files for p4

---
+++ 004/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func max(list []int) (max int) {
+	for _, i := range list {
+		if i > max {
+			max = i
+		}
+	}
+
+	return max
+}
+
+func nums3dig() (list []int) {
+	for i := 100; i < 1000; i++ {
+		list = append(list, i)
+	}
+
+	return list
+}
+
+func reverse(str string) string {
+	n := 0
+	rune := make([]rune, len(str))
+	for _, r := range str {
+		rune[n] = r
+		n++
+	}
+
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+
+	return string(rune)
+}
+
+func checkPalin(i, j int) (res bool) {
+	number := strconv.Itoa(i * j)
+	reversed := reverse(number)
+
+	if strings.Compare(number, reversed) == 0 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	list := nums3dig()
+	var res []int
+
+	for pos, i := range list {
+		for _, j := range list[pos:] {
+			if checkPalin(i, j) {
+				res = append(res, i*j)
+			}
+		}
+	}
+
+	fmt.Println(max(res))
+}
---
+++ 004/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_reverse(t *testing.T) {
+	sample := "Hello"
+	want := "olleH"
+	got := reverse(sample)
+
+	if strings.Compare(want, got) != 0 {
+		t.Errorf("reverse(%s) == %s, want %s", sample, got, want)
+	}
+
+}
+
+func Test_checkPalin(t *testing.T) {
+	num1, num2 := 99, 91 // is 9009
+	want := true
+	got := checkPalin(num1, num2)
+
+	if want != got {
+		t.Errorf("checkPalin(%d, %d) == %v, want %v", num1, num2, got, want)
+
+	}
+
+}
